1e02e806bbbe0ab1e9f5b442b755195980d63336
javaee <javaee@javaee-VirtualBox>
2020-06-14 01:19:21 +0200

    se modifica para que borre datos si entra por GET

--- src/main/java/controllers/ListadoCiudadesController.java
+++ src/main/java/controllers/ListadoCiudadesController.java
@@ -20,51 +20,65 @@ import modelo.pojo.Pais;
  */
 @WebServlet("/listado-ciudades")
 public class ListadoCiudadesController extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
-    
-    public ListadoCiudadesController() {
-        super();
-       
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//crud para operar contra bbdd
+	private static final long serialVersionUID = 1L;
+
+	public ListadoCiudadesController() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// crud para operar contra bbdd
 		CiudadDAOImp dao = CiudadDAOImp.getInstance();
+
+		// lista para guardar todos los paises
+		ArrayList<Ciudad> lista = new ArrayList<Ciudad>();
+
+		// obtiene la session iniciada
+		HttpSession session = request.getSession();
+
+		// si el usuario viene de crear nueva ciudad se borran los datos
+		// que se hayan escrito para que no vuelvan a aparecer en los input
 		
-		//lista para guardar todos los paises
-		ArrayList<Ciudad> lista= new ArrayList<Ciudad>();
+		//datos del formulario update
+		session.removeAttribute("nombreIntroducido");
+		session.removeAttribute("paisSeleccionado");
+		session.removeAttribute("continenteSeleccionado");
 		
-		//obtiene la session iniciada
-		HttpSession session=request.getSession();
-		
-		//se obtiene la lista
+		//datos del formulario de crear
+		session.removeAttribute("nombreIntroducidoC");
+		session.removeAttribute("paisSeleccionadoC");
+		session.removeAttribute("continenteSeleccionadoC");
+
+		// se obtiene la lista
 		try {
-			lista= dao.getAll();
-			
+			lista = dao.getAll();
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			
-			//se pasa la lista guardada en la lista
-			session.setAttribute("lista",lista);
-			
-			//mata la sesion en 1 hora 
-			session.setMaxInactiveInterval(60*60);
-			
-			//se redireciona
+		} finally {
+
+			// se pasa la lista guardada en la lista
+			session.setAttribute("lista", lista);
+
+			// mata la sesion en 1 hora
+			session.setMaxInactiveInterval(60 * 60);
+
+			// se redireciona
 			response.sendRedirect("lista-ciudades.jsp");
 		}
-		
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
